97398a2e75d5c00b62ecd38f72f115d81254eb81
Peter Solymos <psolymos@gmail.com>
2023-04-01 01:48:55 -0600

    Updated
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- R/generative.R
+++ R/generative.R
@@ -27,3 +27,33 @@ for (i in 1:99) {
     fun()
     dev.off()
 }
+
+
+
+fun2 <- function(n=200, rate = 0.05, alpha=0.5) {
+    op <- par(mar=c(0,0,0,0))
+    on.exit(par(op))
+    plot(0, xlim=c(0,1), ylim=c(0,1), ann=FALSE, axes=FALSE, type="n")
+    rect(-1,-1,2,2, 
+        col = apply(col2rgb(sample(colors(), 1, replace = TRUE)), 2, \(x) {
+            rgb(x[1], x[2], x[3], alpha=0.5*255, maxColorValue = 255)
+        }), 
+        border = NA)
+    x0 <- runif(n, -1, 2)
+    y0 <- runif(n, -1, 2)
+    r <- rexp(n, rate)
+    cl <- apply(col2rgb(sample(colors(), n, replace = TRUE)), 2, \(x) {
+            rgb(x[1], x[2], x[3], alpha=alpha*255, maxColorValue = 255)
+        })
+    pch <- sample(21:25, 1)
+    points(x0, y0, 
+        pch=pch, cex=r,
+        col = cl, bg=cl)
+}
+
+set.seed(0)
+for (i in 1:99) {
+    png(paste0("docs/random/app-", if (i < 10) "0" else "", i, ".png"), 200, 200)
+    fun2()
+    dev.off()
+}
